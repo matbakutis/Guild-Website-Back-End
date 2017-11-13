22809bfcafa4fa1fdb5dcef54e13b2acdee39c8b
matbakutis <31048002+matbakutis@users.noreply.github.com>
2017-11-13 15:42:42 -0600

    post works

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -1,16 +1,23 @@
 class ApplicationController < Sinatra::Base
 
-  require 'bundler'
-  Bundler.require
+	require 'bundler'
+	Bundler.require
 
-  register Sinatra::CrossOrigin
+	register Sinatra::CrossOrigin
 
-  ActiveRecord::Base.establish_connection(
-      :adapter => 'postgresql',
-      :database => 'guildy'
-    )
+	ActiveRecord::Base.establish_connection(
+		:adapter => 'postgresql',
+		:database => 'guildy'
+	)
 
-  use Rack::MethodOverride
-  set :method_override, true
+	use Rack::MethodOverride
+	set :method_override, true
+	set :allow_methods, [:get, :post, :options]
+
+	before do
+		headers['Access-Control-Allow-Origin'] = '*'
+		response.headers["Access-Control-Allow-Methods"] = "HEAD,GET,POST,PUT,PATCH,DELETE,OPTIONS"
+	end
+
 
 end
--- controllers/CreateController.rb
+++ controllers/CreateController.rb
@@ -1,17 +1,19 @@
 class CreateController < ApplicationController
 
 	get '/' do
-		
+		'HELLO'
 	end
 
 	post '/' do
+		payload = params 
+		payload = JSON.parse(request.body.read).symbolize_keys
 		@guild = Guild.new
-		@guild.name = params[:name],
-		@guild.realm = params[:realm],
-		@guild.region = params[:region],
-		@guild.about = params[:about]
+		@guild.name = payload[:name]
+		@guild.realm = payload[:realm]
+		@guild.region = payload[:region]
+		@guild.about = payload[:about]
 		@guild.save
-		@guild.id
+		@guild.id.to_s
 	end
 
 end
