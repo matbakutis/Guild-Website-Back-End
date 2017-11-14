6295a1620f4c19f0930a798d63cb884dd1bd3ecf
matbakutis <31048002+matbakutis@users.noreply.github.com>
2017-11-14 16:50:07 -0600

    team post works

--- controllers/CreateController.rb
+++ controllers/CreateController.rb
@@ -4,7 +4,7 @@ class CreateController < ApplicationController
 		'HELLO'
 	end
 
-	post '/' do
+	post '/guild' do
 		payload = params 
 		payload = JSON.parse(request.body.read).symbolize_keys
 		@guild = Guild.new
@@ -13,7 +13,50 @@ class CreateController < ApplicationController
 		@guild.region = payload[:region]
 		@guild.about = payload[:about]
 		@guild.save
-		@guild.id.to_s
+		some_hash = {}
+		some_hash[:id] = @guild.id.to_i
+		some_hash.to_json
+	end
+
+	post '/team' do
+		payload = params 
+		payload = JSON.parse(request.body.read).symbolize_keys
+		@guild = Guild.find_by(id: payload[:guildId])
+		@guild.lf_mythic = payload[:lfMythic]
+		@guild.lf_heroic = payload[:lfHeroic]
+		@guild.lf_rbg = payload[:lfRbg]
+		@guild.about_mythic = payload[:aboutMythic]
+		@guild.about_heroic = payload[:aboutHeroic]
+		@guild.about_rbg = payload[:aboutRbg]
+		@guild.save
+		some_hash = {}
+		some_hash[:id] = @guild.id.to_i
+		some_hash.to_json
+	end
+
+	post '/players' do
+		payload = params 
+		payload = JSON.parse(request.body.read).symbolize_keys
+
+		@player = 
+		
+		@player.save
+		some_hash = {}
+		some_hash[:id] = @.id.to_i
+		some_hash.to_json
 	end
 
 end
+
+player_name
+img_link
+role
+guild_id 
+
+
+
+
+
+
+
+
