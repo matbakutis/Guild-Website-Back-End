6a423521d6e58e412c8104618af45e1a0cdab98f
Collin Cromwell <collcrom@gmail.com>
2017-11-17 10:28:38 -0600

    im going to shoot myself

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -2,13 +2,12 @@ class ApplicationController < Sinatra::Base
 
 	require 'bundler'
 	Bundler.require
-
+	require './config/enviroments'
 	register Sinatra::CrossOrigin
-
-	ActiveRecord::Base.establish_connection(
-		:adapter => 'postgresql',
-		:database => 'guildy'
-	)
+	# ActiveRecord::Base.establish_connection(
+	# 	:adapter => 'postgresql',
+	# 	:database => 'guildy'
+	# )
 
 	use Rack::MethodOverride
 	set :method_override, true
